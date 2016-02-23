e6cebddc7ae12506dba723e83a095d073650054b
Josh Reeves <josh@plyinteractive.com>
2016-02-22 22:44:11 -0800

    Added OR inquiry Widget

--- wp/wp-content/themes/lodge-showcase/page-reservations.php
+++ wp/wp-content/themes/lodge-showcase/page-reservations.php
@@ -38,23 +38,9 @@
     			<?php echo get_field( 'body' ); ?>
     		</div>
 
-        <div id="wufoo-z7x3x5">
-    		Fill out my <a href="http://natapoc.wufoo.com/forms/z7x3x5">online form</a>.
-    		</div>
-    		<script type="text/javascript">var z7x3x5;(function(d, t) {
-    		var s = d.createElement(t), options = {
-    		'userName':'natapoc',
-    		'formHash':'z7x3x5',
-    		'autoResize':true,
-    		'height':'1000',
-    		'async':true,
-    		'header':'show'};
-    		s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'wufoo.com/scripts/embed/form.js';
-    		s.onload = s.onreadystatechange = function() {
-    		var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-    		try { z7x3x5 = new WufooForm();z7x3x5.initialize(options);z7x3x5.display(); } catch (e) {}};
-    		var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-    		})(document, 'script');</script>
+				<!-- OwnerRez Booking/Inquiry widget for Natapoc Lodge -->
+				<div class="ownerrez-widget" data-widgetId="2d144d1be26a46f086014087cce5194c" data-propertyId="c0013024c66049e183caccf5152933ad"></div>
+				<script type="text/javascript" src="https://secure.ownerreservations.com/widget.js"></script>
 
       </div>
     </div>
